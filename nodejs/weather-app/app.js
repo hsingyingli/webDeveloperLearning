ced484186e84b324395ed2ccbd8deb8fd2930e0c
hsingyingli <xingyingli855@gmail.com>
2021-11-07 16:30:11 +0800

    weather app

---
+++ nodejs/weather-app/app.js
@@ -0,0 +1,17 @@
+const request = require('request')
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
+
+
+const address = process.argv[2]
+
+
+geocode(address, (error, {latitude, longitude, location} = {}) => {
+    if (error) return console.log(error)
+
+    forecast(latitude, longitude, (error, forecastData) => {
+        if (error) return console.log('Error: ', error)
+        console.log(`Location: ${location}.`, forecastData)
+    })
+})
+
